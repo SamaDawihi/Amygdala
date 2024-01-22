a42398c3d8df67539ca4d203df35dd2750251e7e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-22 17:20:15 +0000

    Updating to latest FlutterFlow output.

--- lib/home_page/home/home_widget.dart
+++ lib/home_page/home/home_widget.dart
@@ -1,5 +1,5 @@
+import '/components/connection_status/connection_status_widget.dart';
 import '/components/side_nav/side_nav_widget.dart';
-import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
@@ -64,22 +64,15 @@ class _HomeWidgetState extends State<HomeWidget> {
             style: FlutterFlowTheme.of(context).headlineMedium,
           ),
           actions: [
-            Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(12.0, 8.0, 16.0, 8.0),
-              child: FlutterFlowIconButton(
-                borderColor: FlutterFlowTheme.of(context).alternate,
-                borderRadius: 12.0,
-                borderWidth: 2.0,
-                buttonSize: 40.0,
-                fillColor: FlutterFlowTheme.of(context).secondaryBackground,
-                icon: Icon(
-                  Icons.manage_search_rounded,
-                  color: FlutterFlowTheme.of(context).primaryText,
-                  size: 24.0,
+            Container(
+              decoration: const BoxDecoration(),
+              child: SizedBox(
+                width: 200.0,
+                child: wrapWithModel(
+                  model: _model.connectionStatusModel,
+                  updateCallback: () => setState(() {}),
+                  child: const ConnectionStatusWidget(),
                 ),
-                onPressed: () {
-                  print('IconButton pressed ...');
-                },
               ),
             ),
           ],
@@ -98,72 +91,41 @@ class _HomeWidgetState extends State<HomeWidget> {
                 updateCallback: () => setState(() {}),
                 child: const SideNavWidget(),
               ),
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsetsDirectional.fromSTEB(70.0, 0.0, 0.0, 0.0),
-                    child: FFButtonWidget(
-                      onPressed: () {
-                        print('Button pressed ...');
-                      },
-                      text: 'Start a Sesion for Ali',
-                      options: FFButtonOptions(
-                        width: 400.0,
-                        height: 100.0,
-                        padding: const EdgeInsetsDirectional.fromSTEB(
-                            24.0, 0.0, 24.0, 0.0),
-                        iconPadding:
-                            const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                        color: const Color(0x8B4036A4),
-                        textStyle:
-                            FlutterFlowTheme.of(context).titleSmall.override(
-                                  fontFamily: 'Montserrat',
-                                  color: Colors.white,
-                                  fontSize: 30.0,
-                                ),
-                        elevation: 3.0,
-                        borderSide: const BorderSide(
-                          color: Colors.transparent,
-                          width: 1.0,
-                        ),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 100.0, 0.0),
+              Expanded(
                 child: Column(
                   mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        const Icon(
-                          Icons.circle_sharp,
-                          color: Color(0xFF1FA595),
-                          size: 24.0,
-                        ),
-                        Padding(
+                    Padding(
+                      padding:
+                          const EdgeInsetsDirectional.fromSTEB(70.0, 0.0, 0.0, 0.0),
+                      child: FFButtonWidget(
+                        onPressed: () {
+                          print('Button pressed ...');
+                        },
+                        text: 'Start a Sesion for Ali',
+                        options: FFButtonOptions(
+                          width: 400.0,
+                          height: 100.0,
                           padding: const EdgeInsetsDirectional.fromSTEB(
-                              4.0, 0.0, 0.0, 0.0),
-                          child: Text(
-                            'BCI Connected',
-                            style: FlutterFlowTheme.of(context)
-                                .bodyMedium
-                                .override(
-                                  fontFamily: 'Readex Pro',
-                                  color: FlutterFlowTheme.of(context)
-                                      .secondaryText,
-                                  fontWeight: FontWeight.w800,
-                                ),
+                              24.0, 0.0, 24.0, 0.0),
+                          iconPadding: const EdgeInsetsDirectional.fromSTEB(
+                              0.0, 0.0, 0.0, 0.0),
+                          color: const Color(0x8B4036A4),
+                          textStyle:
+                              FlutterFlowTheme.of(context).titleSmall.override(
+                                    fontFamily: 'Montserrat',
+                                    color: Colors.white,
+                                    fontSize: 30.0,
+                                  ),
+                          elevation: 3.0,
+                          borderSide: const BorderSide(
+                            color: Colors.transparent,
+                            width: 1.0,
                           ),
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
-                      ],
+                      ),
                     ),
                   ],
                 ),
